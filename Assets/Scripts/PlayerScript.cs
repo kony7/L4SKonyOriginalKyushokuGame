e9bc5949a292e7fc8a999f03ea7844ea69d9afc0
kony <kony7>
2020-10-03 17:33:17 +0900

    serving false mode, text Request

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,19 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
 
     int score;
     int life = 5;
+    int soupRequestNumber;
+    int sidedishRequestNumber;
 
     float soupAmount;
     float sidedishAmount;
 
     string[] amountRequestText = new string[]{"大盛り","多め","普通","少なめ","少し" };
 
-    bool serving;
+    bool serving = false;
+
+    public GameObject requestText;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +30,15 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(serving == false)
+        {
+            soupRequestNumber = Random.Range(0, 5);
+            sidedishRequestNumber = Random.Range(0, 5);
+            requestText.GetComponent<Text>().text = "スープ" + amountRequestText[soupRequestNumber] + " おかず" + amountRequestText[sidedishRequestNumber];
+        }
+        else
+        {
+
+        }
     }
 }
